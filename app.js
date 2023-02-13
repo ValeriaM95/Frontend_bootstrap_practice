d513590715d185b373716d762aaad17708465b50
Valeriia Muraviova <108786717+ValeriaM95@users.noreply.github.com>
2023-02-13 09:28:59 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const counters = document.querySelectorAll(".counter");
+const box = document.querySelector(".section-1__info");
+let height = window.innerHeight * 2.4;
+const boxTop = box.getBoundingClientRect().top;
+
+// SWIPER
+
+let swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// INCREMENTING COUNTER
+
+function increment() {
+  counters.forEach((item) => {
+    item.innerText = "0";
+
+    const incrementNumber = () => {
+      const target = +item.getAttribute("data-target");
+      const c = +item.innerText;
+      const incrementN = target / 50;
+      if (c < target) {
+        item.innerText = `${c + incrementN}`;
+
+        setTimeout(incrementNumber, 10);
+      }
+    };
+
+    incrementNumber();
+  });
+
+  return;
+}
+
+window.addEventListener("scroll", () => {
+  if (height > boxTop) {
+    increment();
+  }
+});
